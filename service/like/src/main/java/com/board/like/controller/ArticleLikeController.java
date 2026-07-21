b1b8d388333c826a433c6b4421ebe47c19a4709f
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-07-21 15:03:04 +0900

    feat: implements article like service with pessimistic and optimistic locks

---
+++ service/like/src/main/java/com/board/like/controller/ArticleLikeController.java
@@ -0,0 +1,82 @@
+package com.board.like.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.board.like.serivce.ArticleLikeService;
+import com.board.like.serivce.response.ArticleLikeResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ArticleLikeController {
+
+	private final ArticleLikeService articleLikeService;
+
+	@GetMapping("/v1/article-likes/articles/{articleId}/users/{userId}")
+	public ArticleLikeResponse read(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		return articleLikeService.read(articleId, userId);
+	}
+
+	@GetMapping("/v1/article-likes/articles/{articleId}/count")
+	public Long count(
+		@PathVariable("articleId") Long articleId
+	) {
+		return articleLikeService.count(articleId);
+	}
+
+	@PostMapping("/v1/article-likes/articles/{articleId}/users/{userId}/pessimistic-lock-1")
+	public void likePessimisticLock1(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		articleLikeService.likePessimisticLock1(articleId, userId);
+	}
+
+	@DeleteMapping("/v1/article-likes/articles/{articleId}/users/{userId}/pessimistic-lock-1")
+	public void unlikePessimisticLock1(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		articleLikeService.unlikePessimisticLock1(articleId, userId);
+	}
+
+	@PostMapping("/v1/article-likes/articles/{articleId}/users/{userId}/pessimistic-lock-2")
+	public void likePessimisticLock2(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		articleLikeService.likePessimisticLock2(articleId, userId);
+	}
+
+	@DeleteMapping("/v1/article-likes/articles/{articleId}/users/{userId}/pessimistic-lock-2")
+	public void unlikePessimisticLock2(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		articleLikeService.unlikePessimisticLock2(articleId, userId);
+	}
+
+	@PostMapping("/v1/article-likes/articles/{articleId}/users/{userId}/optimistic-lock")
+	public void likeOptimisticLock(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		articleLikeService.likeOptimisticLock(articleId, userId);
+	}
+
+	@DeleteMapping("/v1/article-likes/articles/{articleId}/users/{userId}/optimistic-lock")
+	public void unlikeOptimisticLock(
+		@PathVariable("articleId") Long articleId,
+		@PathVariable("userId") Long userId
+	) {
+		articleLikeService.unlikeOptimisticLock(articleId, userId);
+	}
+}
---
+++ service/like/src/main/java/com/board/like/repository/ArticleLikeRepository.java
@@ -0,0 +1,14 @@
+package com.board.like.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.board.like.entity.ArticleLike;
+
+@Repository
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+
+	Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+}
